f28d4698e388a9993de0ccfa82d6375d66bd541c
Hichem ALI MAHDI <hichem.alimahdi@gmail.com>
2025-10-13 18:12:37 +0000

    Starting server function and start the server implemented

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -19,3 +19,21 @@ fastify.get('/', async (request: FastifyRequest, reply: FastifyReply) => {
     };
 });
 
+const start = async (): Promise<void> => {
+    try {
+        const HOST = process.env.HOST || '0.0.0.0';
+        const PORT = parseInt(process.env.PORT || '3000');
+
+        await fastify.listen({
+            host: HOST,
+            port: PORT
+        });
+        console.log(`Server running at http://${HOST}:${PORT}`);
+    } catch (err) {
+        fastify.log.error(err);
+        process.exit(1);
+    }
+};
+
+
+start();
