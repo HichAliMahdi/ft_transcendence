3d923b1745cc7f1c142ff60c4be0fb1025044f14
Hichem ALI MAHDI <hichem.alimahdi@gmail.com>
2025-10-13 18:05:05 +0000

    Basic health check route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,3 +3,8 @@ import Fastify, { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify'
 const fastify: FastifyInstance = Fastify({
     logger: true
 });
+
+fastify.get('./health', async (request: FastifyRequest, reply: FastifyReply) => {
+    return {status: 'OK', message: 'server is running!'};
+});
+
