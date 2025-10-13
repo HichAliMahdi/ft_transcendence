19785d78a5d1b23e3ecbdd2f895d755fc6c72e79
Hichem ALI MAHDI <hichem.alimahdi@gmail.com>
2025-10-13 18:08:03 +0000

    Basic root route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,3 +8,14 @@ fastify.get('./health', async (request: FastifyRequest, reply: FastifyReply) => 
     return {status: 'OK', message: 'server is running!'};
 });
 
+fastify.get('/', async (request: FastifyRequest, reply: FastifyReply) => {
+    return {
+        message: 'Welcome to my transcendence API',
+        endpoints: {
+            health: '/health',
+            users: '/users (TODO)',
+            game: '/game (TODO'
+        }
+    };
+});
+
