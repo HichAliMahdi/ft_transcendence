2269660fb9666422df76169a5cfad51ad0f2ae62
Hichem ALI MAHDI <hichem.alimahdi@gmail.com>
2025-10-14 14:15:46 +0000

    backend Interface User added

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -1,2 +1,11 @@
 import { db } from '../database/db';
 
+export interface User {
+    id: number;
+    username: string;
+    email?: string;
+    password_hash?: string;
+    avatar_url?: string;
+    created_at: string;
+    updated_at: string;
+}
