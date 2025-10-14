16f94d64fe9b0c0f3f65fb11dee99667513c3d18
Hichem ALI MAHDI <hichem.alimahdi@gmail.com>
2025-10-14 13:55:03 +0000

    Table useres created

--- backend/src/database/db.ts
+++ backend/src/database/db.ts
@@ -1,2 +1,21 @@
 import Database from 'better-sqlite3';
 import { config } from '../config';
+
+export const db = new Database(config.database.path);
+
+export function initializeDatabase(): void {
+    db.exec(
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT UNIQUE NOT NULLm
+            email TEXT UNIQUE,
+            password_has TEXT,
+            avatar_url TEXT DEFAULT '/default-avatar.png',
+            created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+            updated_at DATETIME DEFAULT CURRENT_TIMESTAMP
+        )
+    );
+
+    db.exec(
+        
+    )
