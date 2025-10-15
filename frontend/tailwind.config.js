dc8de06231b3cc8914f4a91784e4df358cef2d0b
Hichem ALI MAHDI <hichem.alimahdi@gmail.com>
2025-10-15 16:54:07 +0000

    Starting migration to tailwind: adding config for tailwind and postcss

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,html}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          dark: '#1f1c2c',
+          light: '#928dab',
+        },
+        accent: {
+          pink: '#ff6ec4',
+          purple: '#7873f5',
+        },
+        game: {
+          dark: '#0f3460',
+          red: '#e94560',
+        }
+      },
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
