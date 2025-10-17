01339e35c12ef1aeb50f9c4addd1ccec17ce07ca
Hichem ALI MAHDI <hichem.alimahdi@gmail.com>
2025-10-17 15:41:50 +0000

    Multiplayer: initialized the paddles, next step: Setup controls and canvas

--- frontend/src/components/game/MultiPlayerPongGame.ts
+++ frontend/src/components/game/MultiPlayerPongGame.ts
@@ -39,18 +39,78 @@ export class MultiplayerPongGame {
     constructor(canvas: HTMLCanvasElement) {
         this.canvas = canvas;
         const context = canvas.getContext('2d');
-        if (!context)
+        if (!context){
             throw new Error('Could not get canvas context');
+        }
         this.ctx = context;
+
+        // Initializing ball in the center of the game
+        this.ball = {
+            x: canvas.width / 2,
+            y: canvas.height / 2,
+            dx: 4,
+            dy: 4,
+            radius: 8
+        };
+
+        // initialize the paddles
+        this.paddles = [
+            {
+                // Top paddle
+                x: canvas.width / 2 - 50,
+                y: 20,
+                width: 100,
+                height: 10,
+                speed: 8,
+                playerId: 1,
+                keys: { up: 'a', down: 'd' }
+            },
+            {
+                // Right paddle
+                x: canvas.width - 30,
+                y: canvas.height / 2 - 50,
+                width: 10,
+                height: 100,
+                speed: 8,
+                playerId: 2,
+                keys: { up: 'ArrowUp', down: 'ArrowDown' }
+            },
+            {
+                // Bottom paddle
+                x: canvas.width / 2 - 50,
+                y: canvas.height - 30,
+                width: 100,
+                height: 10,
+                speed: 8,
+                playerId: 3,
+                keys: { up: 'j', down: 'l' }
+            },
+            {
+                // Left paddle
+                x: 20,
+                y: canvas.height / 2 - 50,
+                width: 10,
+                height: 100,
+                speed: 8,
+                playerId: 4,
+                keys: { up: 'w', down: 's' }
+            },
+        ];
+        this.score = {
+            player1: 0,
+            player2: 0,
+            player3: 0,
+            player4: 0
+        };
+        this.setupControls();
+        this.resizeCanvas();
     }
 
-    // Initializing ball in the center of the game
-    this.ball = {
-        x: canvas.width / 2,
-        y: canvas.height / 2,
-        dx: 4,
-        dy: 4,
-        radius: 8
-    };
+    private setupControls(): void {
+        
+    }
     
+    private resizeCanvas(): void {
+
+    }
 }
